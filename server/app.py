7e8e5c8f5837d5bf85669175d36a01a4f88ed22f
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-12-06 17:54:46 -0500

    add api.add_resource for all routes to app.py

--- server/app.py
+++ server/app.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python3
-
-# Standard library imports
-
-# Remote library imports
 from flask import request
 from flask_restful import Resource
-
-# Local imports
 from config import app, db, api
-# Add your model imports
+
+# route imports
+from routes.all_resorts import Resorts
+from routes.events_routes import Events
+from routes.resort_by_id import ResortByID
+from routes.user_bookmarks import UserBookmarks
+from routes.resort_events import ResortEvents
+from routes.resort_review import ResortReview
+from routes.user_events import UserEvents
 
 
-# Views go here!
+# Routes
+# Get all resorts
+api.add_resource(Resorts, '/resorts')
+
+# Get resort by ID
+api.add_resource(ResortByID, '/resorts/<int:id>')
+
+# Post an event to a resort
+api.add_resource(ResortEvents, '/resort/<int:resort_id>/event/<int:event_id>')
+
+# Post / Delete / Put a review for a resort
+api.add_resource(ResortReview, '/resort/<int:id>/review')
+
+# Get / Post / Delete a user bookmark
+api.add_resource(UserBookmarks, '/user/<int:user_id>/bookmark/<int:resort_id>')
+
+# Get / Post / Delete a user event
+api.add_resource(UserEvents, '/user/<int:user_id>/bookmark/<int:resort_id>')
+
+# Get all types of events
+api.add_resource(Events, '/events')
 
 @app.route('/')
 def index():
@@ -19,5 +41,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    # app.run(port=5555, debug=True)
+    print (Resorts)
 
