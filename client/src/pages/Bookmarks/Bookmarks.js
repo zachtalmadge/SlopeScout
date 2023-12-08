125795def03ab8f9fd753b8292e3c5d4aeed9163
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-12-08 13:40:59 -0500

    add useFetch to Bookmarks.js

--- client/src/pages/Bookmarks/Bookmarks.js
+++ client/src/pages/Bookmarks/Bookmarks.js
@@ -1,6 +1,12 @@
 import ModelMasthead from "../../components/ModelMasthead/ModelMasthead";
+import ResortCard from "../../components/ResortCard";
+import useFetch from "../../hooks/useFetch";
+
+const URL = "http://127.0.0.1:5555/user/1/bookmark/1"
 
 const Bookmarks = () => {
+
+    const { data } = useFetch(`${URL}`)
 
     return (
         <ModelMasthead text="Bookmarks" />
