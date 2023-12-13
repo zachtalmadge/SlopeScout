952d18d9d884e015b285ee86c380f80f1c2c4ecd
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-12-13 14:33:15 -0500

    add email.js with full functionality / validations

---
+++ client/src/components/EventModal/EventModal.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { Modal, Button, Loader, Dimmer, Input } from 'semantic-ui-react';
+import emailjs from '@emailjs/browser';
+
+const EventModal = ({ sendEmail, eventName, eventTime, onClose }) => {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [isValidEmail, setIsValidEmail] = useState(true);
+
+  function formatDateTime(dateTimeString) {
+    const date = new Date(dateTimeString);
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const month = monthNames[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    
+    const formattedDate = `${month} ${day}, ${year}`;
+    const formattedTime = `${hours}:${minutes}`;
+    
+    return { formattedDate, formattedTime };
+}
+
+const { formattedDate, formattedTime } = formatDateTime(eventTime)
+
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setIsValidEmail(true);
+  };
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleRegister = () => {
+    if (!validateEmail()) {
+      setIsValidEmail(false);
+      return;
+    }
+
+    setLoading(true);
+
+    setTimeout(() => {
+      sendEmail();
+      setLoading(false);
+      onClose(); // Close the modal
+    }, 3000); // Spinner shows for 3 seconds
+  };
+
+  function sendEmail(){
+    emailjs.send("service_fb627gb","template_tbhywff",{
+        event: eventName,
+        time: formattedTime,
+        date: formattedDate,
+        user_email: email
+        }, 'Q3vqku9WBPRijR95K');
+  }
+
+  return (
+    <Modal open={true} onClose={onClose}>
+      <Modal.Header>Registered for {eventName}!</Modal.Header>
+      <Modal.Content>
+        <p>Would you like an email confirmation?</p>
+        <Input
+          placeholder="Enter your email"
+          value={email}
+          onChange={handleEmailChange}
+          error={!isValidEmail}
+        />
+        {!isValidEmail && (
+          <p style={{ color: 'red' }}>Please input a valid email</p>
+        )}
+      </Modal.Content>
+      <Modal.Actions>
+        <Button onClick={onClose} color='red' inverted>
+          No thanks
+        </Button>
+        <Button color='primary' onClick={handleRegister} disabled={loading}>
+          {loading ? (
+            <Dimmer active inverted>
+              <Loader size='small'>Sending Email...</Loader>
+            </Dimmer>
+          ) : (
+            'Send Email'
+          )}
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
+};
+
+export default EventModal;
--- client/src/components/ResortDetailsPanes/ResortDetailsPanes.js
+++ client/src/components/ResortDetailsPanes/ResortDetailsPanes.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import { useState } from 'react';
 import { Tab, Button, Card } from 'semantic-ui-react';
 import renderRatingStars from '../../util/renderRatingStars';
 import DateTimeDisplay from '../DateTimeDisplay';
 import sortByDate from '../../util/sortByDate';
 import { useTheme } from '../../contexts/ThemeProvider';
+import EventModal from '../EventModal/EventModal';
 
 
 const ResortDetailsPanes = ({ reviews, events, addToUserEvents }) => {
 
     const { theme } = useTheme();
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleEventClick = (event) => {
+        addToUserEvents(event)
+        setIsModalOpen(true)
+    }
+
     const paneStyle = {
         backgroundColor: theme === 'light' ? 'white' : '#1B1C1D', // Dark mode background
         color: theme === 'light' ? 'black' : 'white', // Dark mode text color
-      };
+    };
 
 
     const reviewPanes = {
@@ -22,9 +30,9 @@ const ResortDetailsPanes = ({ reviews, events, addToUserEvents }) => {
             <Tab.Pane style={paneStyle}>
                 {reviews && reviews.map((review, index) => (
                     <Card key={index}>
-                        <Card.Content style={{backgroundColor: theme === 'light' ? 'white' : "#1B1C1D"}}>
-                            <Card.Description style={{color: theme === 'light' ? "" : 'white'}}>{review.text}</Card.Description>
-                            <hr/>
+                        <Card.Content style={{ backgroundColor: theme === 'light' ? 'white' : "#1B1C1D" }}>
+                            <Card.Description style={{ color: theme === 'light' ? "" : 'white' }}>{review.text}</Card.Description>
+                            <hr />
                             <p>Rating: {renderRatingStars(review.rating)}</p>
                         </Card.Content>
                     </Card>
@@ -41,18 +49,29 @@ const ResortDetailsPanes = ({ reviews, events, addToUserEvents }) => {
         render: () => (
             <Tab.Pane style={paneStyle}>
                 {events && sortByDate(events).map((event, index) => (
-                    <Card key={index}>
-                        <Card.Content style={{backgroundColor: theme === 'light' ? 'white' : "#1B1C1D"}}>
-                            <Card.Header style={{color:"rgb(33,133,208)"}}>{event.event.name}</Card.Header>
-                            <Card.Description style={{color: theme === 'light' ? "" : 'white'}}>{event.event.description}</Card.Description>
-                        </Card.Content>
-                        <Card.Content extra style={{backgroundColor: theme === 'light' ? 'white' : "#1B1C1D"}}> 
-                            <DateTimeDisplay dateTimeString={event.time} theme={theme} />
-                            <Button primary onClick={() => addToUserEvents(event)} inverted={theme === 'dark'}>
-                                Register
-                            </Button>
-                        </Card.Content>
-                    </Card>
+                    <>
+                        <Card key={index}>
+                            <Card.Content style={{ backgroundColor: theme === 'light' ? 'white' : "#1B1C1D" }}>
+                                <Card.Header style={{ color: "rgb(33,133,208)" }}>{event.event.name}</Card.Header>
+                                <Card.Description style={{ color: theme === 'light' ? "" : 'white' }}>{event.event.description}</Card.Description>
+                            </Card.Content>
+                            <Card.Content extra style={{ backgroundColor: theme === 'light' ? 'white' : "#1B1C1D" }}>
+                                <DateTimeDisplay dateTimeString={event.time} theme={theme} />
+                                <Button primary onClick={() => handleEventClick(event)} inverted={theme === 'dark'}>
+                                    Register
+                                </Button>
+                            </Card.Content>
+                        </Card>
+                        {isModalOpen && (
+                            <EventModal
+                                addToUserEvents={addToUserEvents}
+                                // sendEmail={sendEmail}
+                                eventName={event.event.name}
+                                eventTime={event.time}
+                                onClose={() => setIsModalOpen(false)}
+                            />
+                        )}
+                    </>
                 ))}
             </Tab.Pane>
         ),
